5cff56c83a7efe4feba86d4fe53b1dde328b3268
kayuksel <46922047+kayuksel@users.noreply.github.com>
2019-02-19 14:39:00 +0100

    Further improvements

--- models/blocks.py
+++ models/blocks.py
@@ -8,7 +8,9 @@ class Downblock(nn.Module):
         super(Downblock, self).__init__()
         self.dwconv = nn.Conv2d(channels, channels, groups=channels, stride=2,
                                 kernel_size=kernel_size, padding=1, bias=False)
+
         self.bn = nn.BatchNorm2d(channels)
+
     def forward(self, x):
         return self.bn(self.dwconv(x))
 
@@ -20,8 +22,8 @@ class GEBlock(nn.Module):
         self.conv1 = nn.Sequential(nn.BatchNorm2d(in_planes), nn.ReLU(inplace=True),
             nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False))
 
-        self.conv2 = nn.Sequential(nn.BatchNorm2d(out_planes), nn.ReLU(inplace=True), 
-            nn.Dropout(p=dropRate), nn.Conv2d(out_planes, out_planes, kernel_size=3, padding=1, bias=False))
+        self.conv2 = nn.Sequential(nn.BatchNorm2d(out_planes), nn.ReLU(inplace=True), nn.Dropout(p=dropRate), 
+            nn.Conv2d(out_planes, out_planes, kernel_size=3, padding=1, bias=False))
 
         self.equalInOut = (in_planes == out_planes)
 
@@ -29,22 +31,14 @@ class GEBlock(nn.Module):
             out_planes, kernel_size=1, stride=stride, bias=False) or None
 
         if extra_params:
-            if extent == 0:
-                # Global DW Conv + BN
-                self.downop = Downblock(out_planes, kernel_size=spatial)
-            elif extent == 2:
-                self.downop = Downblock(out_planes)
-            elif extent == 4:
-                self.downop = nn.Sequential(Downblock(out_planes), nn.ReLU(inplace=True), Downblock(out_planes))
-            elif extent == 8:
-                self.downop = nn.Sequential(Downblock(out_planes), nn.ReLU(inplace=True),
-                    Downblock(out_planes), nn.ReLU(inplace=True), Downblock(out_planes))
-            else:
-                raise NotImplementedError('Extent must be 0,2,4 or 8 for now')
+            if extent: modules = [Downblock(out_planes)]
+            for i in range(extent // 2)
+                modules.append(nn.Sequential(nn.ReLU(inplace=True), Downblock(out_planes)))
+            self.downop = nn.Sequential(*modules) if extent else Downblock(out_planes, kernel_size=spatial)
         else:
-            self.downop = nn.AdaptiveAvgPool2d(spatial // extent) if extent else self.downop = nn.AdaptiveAvgPool2d(1)
+            self.downop = nn.AdaptiveAvgPool2d(spatial // extent) if extent else nn.AdaptiveAvgPool2d(1)
 
-        self.mlp = nn.Sequential(nn.Conv2d(out_planes, out_planes // 16, kernel_size=1, padding=0, bias=False), nn.ReLU(),
+        self.mlp = nn.Sequential(nn.Conv2d(out_planes, out_planes // 16, kernel_size=1, padding=0, bias=False), nn.ReLU(inplace=True),
             nn.Conv2d(out_planes // 16, out_planes, kernel_size=1, padding=0, bias=False)) if mlp else lambda x: x
 
     def forward(self, x):
